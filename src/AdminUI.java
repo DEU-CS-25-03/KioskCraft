f663b6c3a73df31a3dfd8220f7a7bde15bf0eaf0
joalstjr <joalstjr@naver.com>
2025-05-26 14:48:17 +0900

    메뉴삭제 추가

--- src/AdminUI.java
+++ src/AdminUI.java
@@ -11,15 +11,48 @@ public class AdminUI extends JFrame{
         SwingUtilities.invokeLater(this::requestFocusInWindow);
         setLayout(null);
 
+        //테이블 선언 + 모델 따로 분리해서 받음
+        DefaultTableModel model = getTableModel();
+        JTable table = new JTable(model);
+
+        //폰트 지정
+        table.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+
+        //열 크기 지정, 취소 버튼 렌더링
+        table.getColumn("카테고리").setPreferredWidth(150);
+        table.getColumn("메뉴").setPreferredWidth(260);
+        table.getColumn("가격").setPreferredWidth(30);
+        table.getColumn("품절여부").setPreferredWidth(30);
+        table.getColumn("").setPreferredWidth(30);
+        table.getColumn("").setCellRenderer(new ButtonRenderer());
+        table.getColumn("").setCellEditor(new ButtonEditor(new JCheckBox(), model));
+
+        //스크롤 추가
+        JScrollPane scrollPane = new JScrollPane(table);
+
+        //행 두께 지정, 위치&크기 지정
+        table.setRowHeight(35);
+        scrollPane.setBounds(10, 70, 830, 500);
+
+        //행 선택 비활성화, 열 위치 조정 비활성화, 열 크기 조정 비활성화,
+        table.setRowSelectionAllowed(false);
+        table.getTableHeader().setResizingAllowed(false);
+        table.getTableHeader().setReorderingAllowed(false);
+
+        //프레임에 추가
+        add(scrollPane);
+
         //버튼 세팅(위치, 크기, 폰트) 이벤트는 추후 추가 예정
         JButton registMenuBtn = new JButton("메뉴 등록");
         registMenuBtn.setBounds(10, 10, 200, 50);
         registMenuBtn.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+        registMenuBtn.addActionListener(_ -> { new MenuControlUI(true, table).setVisible(true); });
         add(registMenuBtn);
 
         JButton modifyMenuBtn = new JButton("메뉴 수정");
         modifyMenuBtn.setBounds(220, 10, 200, 50);
         modifyMenuBtn.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+        modifyMenuBtn.addActionListener(_ -> { new MenuControlUI(false, table).setVisible(true); });
         add(modifyMenuBtn);
 
         JButton orderedCheckBtn = new JButton("매출 분석");
@@ -31,85 +64,65 @@ public class AdminUI extends JFrame{
         setDefaultDesignBtn.setBounds(640, 10, 200, 50);
         setDefaultDesignBtn.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
         add(setDefaultDesignBtn);
+        setDefaultDesignBtn.addActionListener(_ -> { new DesignUI().setVisible(true); });
 
         JButton registCategoryBtn = new JButton("카테고리 등록");
-        registCategoryBtn.setBounds(10, 580, 270, 50);
+        registCategoryBtn.setBounds(10, 580, 200, 50);
         registCategoryBtn.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
         add(registCategoryBtn);
 
         JButton modifyCategoryBtn = new JButton("카테고리 수정");
-        modifyCategoryBtn.setBounds(290, 580, 270, 50);
+        modifyCategoryBtn.setBounds(220, 580, 200, 50);
         modifyCategoryBtn.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
         add(modifyCategoryBtn);
 
         JButton deleteCategoryBtn = new JButton("카테고리 삭제");
-        deleteCategoryBtn.setBounds(570, 580, 270, 50);
+        deleteCategoryBtn.setBounds(430, 580, 200, 50);
         deleteCategoryBtn.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
         add(deleteCategoryBtn);
 
-        //테이블 선언 + 모델 따로 분리해서 받음
-        DefaultTableModel model = getTableModel();
-        JTable table = new JTable(model);
+        JButton showOrderedListBtn = new JButton("주문현황 확인");
+        showOrderedListBtn.setBounds(640, 580, 200, 50);
+        showOrderedListBtn.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
+        add(showOrderedListBtn);
 
-        //폰트 지정
-        table.setFont(new Font("맑은 고딕", Font.PLAIN, 16));
-
-        //열 크기 지정
-        table.getColumn("카테고리").setPreferredWidth(150);
-        table.getColumn("메뉴").setPreferredWidth(290);
-        table.getColumn("가격").setPreferredWidth(30);
-        table.getColumn("").setPreferredWidth(30);
-
-        // 버튼 렌더러, 에디터 추가 (여기서 model 넘겨줌)
-        table.getColumn("").setCellRenderer(new ButtonRenderer());
-        table.getColumn("").setCellEditor(new ButtonEditor(new JCheckBox(), model));
-
-        //스크롤 추가
-        JScrollPane scrollPane = new JScrollPane(table);
-
-        //행 두께 지정
-        table.setRowHeight(35);
-
-        //위치&크기 지정
-        scrollPane.setBounds(10, 70, 830, 500);
-
-        //행 선택 비활성화
-        table.setRowSelectionAllowed(false);
-
-        add(scrollPane);
-
-        //모니터 중앙에 위치
+        //모니터 중앙에 위치, 크기 변경 비활성화
         setLocationRelativeTo(null);
-
-        //크기 변경 기능 해제
         setResizable(false);
     }
 
     // 테이블 모델을 반환 (버튼 에디터에 넘길 목적)
-    private static DefaultTableModel getTableModel() {
+    public static DefaultTableModel getTableModel() {
         Object[][] rowData = {
-                {"커피", "아메리카노", "4,000원", "X"},
-                {"논커피", "딸기라떼", "7,000원", "X"},
-                {"커피", "아메리카노", "4,000원", "X"},
-                {"커피", "카푸치노", "6,000원", "X"},
-                {"논커피", "딸기라떼", "7,000원", "X"},
-                {"커피", "아메리카노", "4,000원", "X"},
-                {"커피", "카푸치노", "6,000원", "X"},
-                {"논커피", "딸기라떼", "7,000원", "X"},
-                {"커피", "아메리카노", "4,000원", "X"},
-                {"커피", "카푸치노", "6,000원", "X"},
-                {"논커피", "딸기라떼", "7,000원", "X"},
-                {"커피", "아메리카노", "4,000원", "X"},
-                {"커피", "카푸치노", "6,000원", "X"},
-                {"논커피", "딸기라떼", "7,000원", "X"}
+                {"커피", "아메리카노", "4,000원", "O"},
+                {"커피", "카페라떼", "4,500원", "X"},
+                {"커피", "카푸치노", "5,000원", "X"},
+                {"커피", "바닐라라떼", "5,000원", "X"},
+                {"커피", "콜드브루", "5,500원", "X"},
+                {"논커피", "딸기라떼", "6,000원", "X"},
+                {"논커피", "녹차라떼", "6,000원", "O"},
+                {"논커피", "초코라떼", "6,000원", "O"},
+                {"논커피", "오렌지주스", "5,000원", "O"},
+                {"논커피", "유자차", "5,500원", "X"},
+                {"디저트", "치즈케이크", "7,000원", "X"},
+                {"디저트", "티라미수", "7,500원", "X"},
+                {"디저트", "마카롱", "2,000원", "X"},
+                {"디저트", "쿠키", "1,500원", "X"},
+                {"디저트", "브라우니", "2,500원", "X"},
+                {"스무디", "딸기스무디", "6,500원", "X"},
+                {"스무디", "망고스무디", "6,500원", "O"},
+                {"스무디", "블루베리스무디", "7,000원", "X"},
+                {"에이드", "레몬에이드", "5,000원", "x"},
+                {"에이드", "자몽에이드", "5,500원", "X"}
         };
-        String[] columnNames = {"카테고리", "메뉴", "가격", ""};
+
+        String[] columnNames = {"카테고리", "메뉴", "가격", "품절여부", ""};
 
         // 테이블 모델 생성
         return new DefaultTableModel(rowData, columnNames) {
             @Override
             public boolean isCellEditable(int row, int column) {
-                return column == 3; // 4번째 열(작업)만 수정 가능(=버튼 클릭 가능)
+                return column == 4; // 4번째 열(작업)만 수정 가능(=버튼 클릭 가능)
             }
         };
     }
@@ -146,6 +159,9 @@ public class AdminUI extends JFrame{
                 SwingUtilities.invokeLater(() -> {
                     if(rowToDelete >= 0 && rowToDelete < model.getRowCount()) {
                         model.removeRow(rowToDelete); // 그 다음에 행 삭제
+                        JOptionPane.showMessageDialog(
+                        null, "메뉴가 삭제되었습니다.", "알림", JOptionPane.INFORMATION_MESSAGE
+                        );
                     }
                 });
             });
