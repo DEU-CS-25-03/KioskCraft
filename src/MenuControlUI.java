8265d8f0533541f1e7748b4b9735cec4c5fe14a4
joalstjr <joalstjr@naver.com>
2025-05-28 11:03:14 +0900

    이미지 변경

--- src/MenuControlUI.java
+++ src/MenuControlUI.java
@@ -18,6 +18,19 @@ public class MenuControlUI extends JFrame {
         setSize(1280, 720);
 
 
+        JLabel menuLabel = new JLabel("메뉴명: ");
+        menuLabel.setBounds(10, 10,100, 10);
+        add(menuLabel);
+        JLabel cateogoryLabel = new JLabel("카테고리: ");
+        menuLabel.setBounds(10, 30,100, 10);
+        add(cateogoryLabel);
+        JLabel priceLabel = new JLabel("가격: ");
+        menuLabel.setBounds(10, 50,100, 10);
+        add(priceLabel);
+        JLabel imgPathLabel = new JLabel("이미지 경로: ");
+        menuLabel.setBounds(10, 70,100, 10);
+        add(imgPathLabel);
+
 
         JButton menuControlBtn = new JButton(btnText);
         menuControlBtn.addActionListener(_ -> JOptionPane.showMessageDialog(null, "메뉴가 " + btnText + " 되었습니다."));
