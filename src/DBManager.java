86cd586ccde589fb1f10867c5bb0d2e5c96ac369
Grrrrra <jyj36900@gmail.com>
2025-05-30 04:20:09 +0900

    Edit : OrderStatusUI 수정 및 예시 DAO,DBManager 클래스 추가 (주석 표시)

--- src/DBManager.java
+++ src/DBManager.java
@@ -1,9 +1,15 @@
-//import DataAccessObject.*;
-//import java.sql.Connection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 //
-//public class DBManager {
+//// DAO import 예시
+//// import DataAccessObject.*;
+
+public class DBManager {
 //    private static DBManager instance;
 //    private Connection connection;
+//
+//    // DAO 인스턴스들
 //    private MenuDAO menuDAO;
 //    private CategoryDAO categoryDAO;
 //    private DesignDAO designDAO;
@@ -14,51 +20,61 @@
 //    private CartDAO cartDAO;
 //    private LanguageDAO languageDAO;
 //
-//    private DBManager() { /* ... */ }
+//    // DB 연결 정보
+//    private final String url = "jdbc:mysql://localhost:3306/cafe";
+//    private final String user = "root";
+//    private final String password = "password";
 //
-//    public static DBManager getInstance() {
+//    private DBManager() {}
+//
+//    public static synchronized DBManager getInstance() {
 //        if (instance == null) instance = new DBManager();
 //        return instance;
 //    }
 //
-//    public void connectDB(String url, String user, String pw) {
-//        // JDBC 드라이버 로딩 및 Connection 생성
-//        // connection = DriverManager.getConnection(url, user, pw);
-//        // 각 DAO에 connection 주입
+//    // DB 연결 및 DAO 인스턴스 생성
+//    public void connectDB() throws SQLException {
+//        if (connection == null || connection.isClosed()) {
+//            connection = DriverManager.getConnection(url, user, password);
+//        }
+//        // DAO 객체 생성 및 Connection 주입
 //        menuDAO = new MenuDAO(connection);
 //        categoryDAO = new CategoryDAO(connection);
-//        // ...
+//        designDAO = new DesignDAO(connection);
+//        paymentRecordDAO = new PaymentRecordDAO(connection);
+//        pointNumberDAO = new PointNumberDAO(connection);
+//        orderStatusDAO = new OrderStatusDAO(connection);
+//        couponDAO = new CouponDAO(connection);
+//        cartDAO = new CartDAO(connection);
+//        languageDAO = new LanguageDAO(connection);
 //    }
 //
+//    public void closeConnection() {
+//        try {
+//            if (connection != null && !connection.isClosed()) {
+//                connection.close();
+//            }
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    // DAO Getter
 //    public MenuDAO getMenuDAO() { return menuDAO; }
 //    public CategoryDAO getCategoryDAO() { return categoryDAO; }
-//    // ... 기타 DAO getter
+//    public DesignDAO getDesignDAO() { return designDAO; }
+//    public PaymentRecordDAO getPaymentRecordDAO() { return paymentRecordDAO; }
+//    public PointNumberDAO getPointNumberDAO() { return pointNumberDAO; }
+//    public OrderStatusDAO getOrderStatusDAO() { return orderStatusDAO; }
+//    public CouponDAO getCouponDAO() { return couponDAO; }
+//    public CartDAO getCartDAO() { return cartDAO; }
+//    public LanguageDAO getLanguageDAO() { return languageDAO; }
 //
-//    public static class DBManager {
-//        private static DBManager instance;
-//        private Connection connection;
-//        private MenuDAO menuDAO;
-//        private CategoryDAO categoryDAO;
-//        // ... 기타 DAO
-//
-//        private DBManager() { /* ... */ }
-//
-//        public static DBManager getInstance() {
-//            if (instance == null) instance = new DBManager();
-//            return instance;
+//    // Connection getter (필요시)
+//    public Connection getConnection() throws SQLException {
+//        if (connection == null || connection.isClosed()) {
+//            connectDB();
 //        }
-//
-//        public void connectDB(String url, String user, String pw) {
-//            // JDBC 드라이버 로딩 및 Connection 생성
-//            // connection = DriverManager.getConnection(url, user, pw);
-//            // 각 DAO에 connection 주입
-//            menuDAO = new MenuDAO(connection);
-//            categoryDAO = new CategoryDAO(connection);
-//            // ...
-//        }
-//        public MenuDAO getMenuDAO() { return menuDAO; }
-//        public CategoryDAO getCategoryDAO() { return categoryDAO; }
-//        // ... 기타 DAO getter
-//
+//        return connection;
 //    }
-//}
+}
