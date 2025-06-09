cc47fd6451c7aeb3b37482139175ff9ebd69008e
Grrrrra <jyj36900@gmail.com>
2025-06-09 21:41:57 +0900

    Edit : entity language 분리

--- src/Controller/LanguageControl.java
+++ src/Controller/LanguageControl.java
@@ -1,32 +1,31 @@
 package Controller;
 
+import DataTransferObject.LanguageEntity;
 import javax.swing.table.DefaultTableModel;
 
 /**
- * DesignControl 클래스
- * - 언어 변경 UI에서 사용될 테이블 모델을 제공
- * - 수정 불가능한 언어 목록을 반환
+ * 언어 변경 UI에서 사용할 테이블 모델을 제공하는 컨트롤러
  */
 public class LanguageControl {
 
     /**
      * getTableModel 메서드
-     * - 언어 목록(한국어, 일본어, 중국어, 영어)을 테이블 모델 형태로 반환
+     * - LanguageEntity.languages에서 언어 목록을 가져와 테이블 모델로 반환
      * - 모든 셀 편집 불가 설정
      */
     public static DefaultTableModel getTableModel() {
-        Object[][] rowData = {
-                { "한국어" },
-                { "일본어" },
-                { "중국어" },
-                { "영어" }
-        };
+        // LanguageEntity에서 언어 목록을 가져와 Object 배열로 변환
         String[] columnNames = { "언어 목록" };
+        Object[][] rowData = new Object[LanguageEntity.languages.size()][1];
+        for (int i = 0; i < LanguageEntity.languages.size(); i++) {
+            rowData[i][0] = LanguageEntity.languages.get(i);
+        }
 
+        // 테이블 모델 생성 및 셀 편집 불가 설정
         return new DefaultTableModel(rowData, columnNames) {
             @Override
             public boolean isCellEditable(int row, int column) {
-                return false; // 셀 편집 불가
+                return false;
             }
         };
     }
