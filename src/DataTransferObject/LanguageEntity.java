0f67a0cbd063a94fedbc9bd17183ca1805c9c250
Grrrrra <jyj36900@gmail.com>
2025-06-09 21:41:57 +0900

    Edit : entity language 분리

---
+++ src/DataTransferObject/LanguageEntity.java
@@ -0,0 +1,17 @@
+package DataTransferObject;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 언어 목록을 저장하는 데이터 캐시 클래스
+ */
+public class LanguageEntity {
+    // 언어 목록 리스트
+    public static final List<String> languages = Arrays.asList(
+            "한국어",
+            "일본어",
+            "중국어",
+            "영어"
+    );
+}
